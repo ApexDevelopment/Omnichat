6b50c885170376c5cb573e4665c72685d29ee3fb
Micah Havens <micah.apex@gmail.com>
2022-10-16 21:46:06 -0400

    Implement socket.io example chat
    This adds the "getting started" example from socket.io's
    documentation, with some modifications in preparation for
    architecting the code around Omni. A dependency on socket.io has
    been added. A stylesheet has also been added.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 const omni = require("omni");
+const http = require("http");
 const express = require("express");
 const app = express();
+const server = http.createServer(app);
 const port = 80;
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 const path = require("path");
 const public_directory = path.join(__dirname, "public");
 
 app.use(express.static(public_directory));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+	console.log("New user connection!");
+
+	socket.on("disconnect", () => {
+		console.log("User disconnected!");
+	});
+
+	socket.on("msg_send", (data) => {
+		console.log("Message received: " + data.message);
+		// TODO: rearchitect to use socket.broadcast.emit
+		io.emit("msg_rcv", data);
+	});
+});
+
+
+
+server.listen(port, () => {
 	console.log(`Server started at port ${port}`);
 });
